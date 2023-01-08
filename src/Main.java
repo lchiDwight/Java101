5cf561cf9e67a72762d43010a9d3a516cabc7c62
lichi <lichi.liqi@gmail.com>
2023-01-08 15:42:30 +0900

    checking

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Uploading v1");
+        System.out.println("Still testing GitHub");
         System.out.println("Hello world!");
     }
 }
