ec42d41465a5e2b681d70a3813ceacd07409a216
lichi <lichi.liqi@gmail.com>
2023-01-09 14:29:05 +0900

    save new task

---
+++ src/NewTask.java
@@ -0,0 +1,12 @@
+public class NewTask {
+
+//     you need to delare a variable
+
+//    you need to delare a String variable
+
+//    You need to make a math calculation
+
+    public static void newTaskDetail(){
+        System.out.println("We have a new task");
+    }
+}
