9dcbe2d18f05173aed4abca25bb437f5bd6551d6
lichi <lichi.liqi@gmail.com>
2023-01-09 15:40:02 +0900

    ok

--- src/Task3.java
+++ src/Task3.java
@@ -1,11 +1,10 @@
 public class Task3 {
   
-  public static String taskName = "taks 3";
-  
+  public static String taskName = "task 3";
+
   public static void taskStart(){
     
     System.out.println("hello");
-    
   }
-  
+
 }
