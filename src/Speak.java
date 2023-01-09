a58611459bbe4dace9a180c2eedcf339c72e3415
lichi <lichi.liqi@gmail.com>
2023-01-09 13:38:17 +0900

    deleted two files

--- src/Speak.java
+++
@@ -1,7 +0,0 @@
-public class Speak {
-    public static String words = "Hello I'm speaking";
-
-    public static void saySomething(){
-        System.out.println(words);
-    }
-}
--- src/Test.java
+++
@@ -1,4 +0,0 @@
-public class Test {
-    public static String sayHi = "Hello there";
-
-}
